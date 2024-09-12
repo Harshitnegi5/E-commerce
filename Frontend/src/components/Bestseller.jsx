5f2cbbdf72d0bf15e19cfa0c64bbc9bfe4babbe8
Harshit Negi <141933477+Harshitnegi5@users.noreply.github.com>
2024-09-12 17:55:01 +0530

    Add bestseller comp

---
+++ Frontend/src/components/Bestseller.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const Bestseller = () => {
+  const { products } = useContext(ShopContext);
+
+  const [bestSeller, setBestseller] = useState([]);
+
+  useEffect(() => {
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestseller(bestProduct.slice(0, 5));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+          vitae voluptatibus eveniet quibusdam quaerat cupiditate!
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 ">
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Bestseller;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import Bestseller from '../components/Bestseller'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <LatestCollection/>
+      <Bestseller/>
     </>
   )
 }
