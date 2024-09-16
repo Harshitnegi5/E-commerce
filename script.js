cf7f431c807b1f8c3a4805355901c83b90f184ae
Harshit Negi <141933477+Harshitnegi5@users.noreply.github.com>
2024-09-16 14:38:54 +0530

    modify

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".nav > img").addEventListener("click",()=>{
-    document.querySelector(".page1 > .menu").classList.toggle("open")
-})
-
-document.querySelector(".page1 > .menu").addEventListener("click",()=>{
-    document.querySelector(".page1 > .menu").classList.remove("open")
-})
